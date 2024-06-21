8a8da3a5e4eaa8a585976fb3069299bebfce6c28
WIN-RG2PSV1K1AK\user <mc.langelo@yandex.ru>
2024-06-21 22:46:51 +0300

    slow fixes

---
+++ src/masks.py
@@ -0,0 +1,10 @@
+def get_mask_card_number(card_number: str) -> str | None:
+    """Функция принимает на вход номер карты и возвращает ее маску"""
+    masked_number = card_number[:4] + " " + "** **** " + card_number[-4:]
+    return masked_number
+
+
+def get_mask_account(account_number: str) -> str | None:
+    """Функция принимает на вход номер счета и возвращает его маску"""
+    masked_number = "**" + account_number[-4:]
+    return masked_number
