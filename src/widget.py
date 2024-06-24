39b76b68549003f4251a4d92a361135e680f80d4
WIN-RG2PSV1K1AK\user <mc.langelo@yandex.ru>
2024-06-24 22:29:04 +0300

    правильный вариант

--- src/widget.py
+++ src/widget.py
@@ -8,7 +8,7 @@ def mask_account_card(input_string: str) -> str | None:
     elif "Счет" in input_string:
         return get_mask_account(input_string)
     else:
-        return Noneпше
+        return None
 
 
 def get_data(input_string: str) -> str | None:
