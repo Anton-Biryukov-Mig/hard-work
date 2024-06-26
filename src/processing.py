9db47e1a147b8842b1a3f75e29e99a32f9ed54f6
Anton_Biryukov <mc.langelo@yandex.ru>
2024-06-27 00:41:56 +0300

    добавление модуля processing

---
+++ src/processing.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from typing import Any, Dict, List
+
+
+def filter_by_state(data: List[Dict[str, Any]], state: str) -> List[Dict[str, Any]]:
+    """Функция принимает на вход список словарей и значение для ключа state."""
+    return [item for item in data if item.get("state") == state]
+
+
+def sort_by_date(data: List[Dict[str, Any]], reverse: bool = False) -> List[Dict[str, Any]]:
+    """Функция принимает на вход список словарей и возвращает новый список."""
+    return sorted(data, key=lambda x: datetime.strptime(x["date"], "%Y-%m-%dT%H:%M:%S.%f"), reverse=reverse)
