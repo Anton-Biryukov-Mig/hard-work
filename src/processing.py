43986301aeade88eb90c02d7c5ca676031aa9e51
Anton_Biryukov <mc.langelo@yandex.ru>
2024-06-28 18:50:25 +0300

    пытаюсь создать PR

--- src/processing.py
+++ src/processing.py
@@ -10,4 +10,3 @@ def filter_by_state(data: List[Dict[str, Any]], state: str) -> List[Dict[str, An
 def sort_by_date(data: List[Dict[str, Any]], reverse: bool = False) -> List[Dict[str, Any]]:
     """Функция принимает на вход список словарей и возвращает новый список."""
     return sorted(data, key=lambda x: datetime.strptime(x["date"], "%Y-%m-%dT%H:%M:%S.%f"), reverse=reverse)
-
