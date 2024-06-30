66140d523521679db409644e08a626080ae596b5
Anton_Biryukov <mc.langelo@yandex.ru>
2024-06-30 14:18:37 +0300

    изменил функцию filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -2,9 +2,10 @@ from datetime import datetime
 from typing import Any, Dict, List
 
 
-def filter_by_state(data: List[Dict[str, Any]], state: str) -> List[Dict[str, Any]]:
-    """Функция принимает на вход список словарей и значение для ключа state."""
-    return [item for item in data if item.get("state") == state]
+def filter_by_state(data: List[Dict[str, Any]], state: str = "EXECUT", ascending: bool = True) -> List[Dict[str, Any]]:
+    """Функцию принимает на вход список словарей и значение для ключа state."""
+    filtered_data = [item for item in data if item.get("state") == state]
+    return sorted(filtered_data, key=lambda x: x.get("state", ""), reverse=not ascending)
 
 
 def sort_by_date(data: List[Dict[str, Any]], reverse: bool = False) -> List[Dict[str, Any]]:
