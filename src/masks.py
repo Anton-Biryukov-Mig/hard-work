b694a3ff50b5c1e6e22ca823bbb6472c3d5f1d0e
Anton_Biryukov <mc.langelo@yandex.ru>
2024-07-10 01:04:27 +0300

    новые изменения

--- src/masks.py
+++ src/masks.py
@@ -1,21 +1,27 @@
+import re
+
+
 def get_mask_card_number(card_info: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску"""
     card_info = card_info.replace(" ", "")
-    for i in range(len(card_info)):
-        if card_info[i].isdigit():
+    enumeration = None
+    for i, char in enumerate(card_info):
+        if char.isdigit():
             enumeration = i
             break
 
-    card_type = card_info[:enumeration]
-    card_number = card_info[enumeration:]
+    if enumeration is None:
+        return "Неверный формат входных данных"
+
+    card_type, card_number = card_info[:enumeration], card_info[enumeration:]
     if len(card_number) != 16:
         return "Неверный формат входных данных"
 
+    card_type_with_spaces = re.sub(r"([A-Z])", r" \1", card_type).strip()
     masked_number = card_number[0:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
-    return card_type + " " + masked_number
+    return f"{card_type_with_spaces} {masked_number}"
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция принимает на вход номер счета и возвращает его маску"""
-    masked_account = f"**{account_number[-4:]}"
-    return masked_account
+    return "Счет **" + account_number[-4:]
