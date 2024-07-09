d70f38f8d9b677ce0baa560e15db7766394577f8
Anton_Biryukov <mc.langelo@yandex.ru>
2024-07-10 02:07:17 +0300

    новые изменения 1

--- src/processing.py
+++ src/processing.py
@@ -11,3 +11,4 @@ def filter_by_state(data: List[Dict[str, str]], state: str = "EXECUTED") -> List
 def sort_by_date(data: List[Dict[str, Any]], reverse: bool = True) -> List[Dict[str, Any]]:
     """Функция принимает на вход список словарей и возвращает новый список."""
     return sorted(data, key=lambda x: datetime.strptime(x["date"], "%Y-%m-%dT%H:%M:%S.%f"), reverse=reverse)
+
