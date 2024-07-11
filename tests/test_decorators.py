e5cc74ba9b5db6d2f95344b3f5c5750a0bef1679
Anton_Biryukov <mc.langelo@yandex.ru>
2024-07-11 21:35:52 +0300

    тест декораторы

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -7,34 +7,17 @@ from src.decorators import log
 
 @log(filename="src\\mylog.txt")  # Using double backslashes
 def my_function(x: int, y: int) -> int:
-    """Add two integers and return the result.
-
-    Args:
-        x (int): The first integer.
-        y (int): The second integer.
-
-    Returns:
-        int: The sum of x and y.
-    """
+    """Сложите два целых числа и верните результат"""
     return x + y
 
 
 @pytest.mark.parametrize("filename", [None, "src\\mylog.txt"])  # Using double backslashes
 def test_log(capsys: Any, filename: Optional[str]) -> None:
-    """Test the log decorator.
-
-    Args:
-        capsys (Any): The pytest fixture for capturing stdout/stderr.
-        filename (Optional[str]): The filename for logging.
-    """
+    """Тест log"""
 
     @log(filename)
     def test_func() -> None:
-        """Test function.
-
-        Returns:
-            None
-        """
+        """Тест func"""
         my_function(1, 2)
 
     test_func()
