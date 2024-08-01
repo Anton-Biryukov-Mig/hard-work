2142a1c0652ed011378df3c0068a2d7306edd6c2
Anton_Biryukov <mc.langelo@yandex.ru>
2024-08-01 20:04:52 +0300

    external_api

---
+++ src/external_api.py
@@ -0,0 +1,38 @@
+import os
+from typing import Any, Dict
+
+import requests
+from dotenv import load_dotenv
+
+from src.utils import get_data_transactions
+
+load_dotenv("C:/Projects/FirstPython/my_prj/anton_biryukov_home_work/.env")
+
+path = "C:/Projects/FirstPython/my_prj/anton_biryukov_home_work/data/operations.json"
+
+
+def amount_transaction(transaction_by_id: Dict[str, Any]) -> float:
+    """Функция возвращает сумму транзакции в рублях"""
+    trans_amount = transaction_by_id["operationAmount"]["amount"]
+    trans_code = transaction_by_id["operationAmount"]["currency"]["code"]
+    api_key = os.getenv("API_KEY")
+    headers_api = {"apikey": api_key}
+    if trans_code == "RUB":
+        return float(trans_amount)
+    else:
+        try:
+            url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={trans_code}&amount={trans_amount}"
+            response = requests.request("GET", url, headers=headers_api)
+            my_result = response.json()
+            return float(my_result["result"])
+        except Exception as e:
+            print(e)
+            return 0.0  # Default return value in case of an exception
+
+
+if __name__ == "__main__":
+    path = "C:/Projects/FirstPython/my_prj/anton_biryukov_home_work/data/operations.json"
+    transactions = get_data_transactions(path)
+    n = int(input("введите колличество транзакций: "))
+    for i in range(n):
+        print(amount_transaction(transactions[i]))
