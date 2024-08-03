18c05a6aa282a8cdd01f2a059dc9b8329bea67b4
Anton_Biryukov <mc.langelo@yandex.ru>
2024-08-03 12:39:14 +0300

    логирование модуля masks

--- src/masks.py
+++ src/masks.py
@@ -1,9 +1,20 @@
+import logging
 import re
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler(
+    "C:/Projects/FirstPython/my_prj/anton_biryukov_home_work/logs/masks.log", encoding="utf-8"
+)
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
 
 
 def get_mask_card_number(card_info: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску"""
-    card_info = card_info.replace(" ", "")
+    logger.info("маскируем банковскую карту клиента")
+    card_info = str(card_info).replace(" ", "")
     enumeration = None
     for i, char in enumerate(card_info):
         if char.isdigit():
@@ -24,4 +35,10 @@ def get_mask_card_number(card_info: str) -> str:
 
 def get_mask_account(account_number: str) -> str:
     """Функция принимает на вход номер счета и возвращает его маску"""
+    logger.info("маскируем банковский счет клиента")
     return "Счет **" + account_number[-4:]
+
+
+if __name__ == "__main__":
+    print(get_mask_card_number("Maestro 1596837868705199"))
+    print(get_mask_account("64686473678894779589"))
